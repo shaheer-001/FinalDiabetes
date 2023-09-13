84ac4e5ecb7872c7b08d823d6d20ea6b3ad2503a
shaheer-001 <114086096+shaheer-001@users.noreply.github.com>
2023-09-13 21:01:18 +0500

    Add files via upload

---
+++ InternshipProject-main/app.py
@@ -0,0 +1,21 @@
+from flask_ngrok import run_with_ngrok
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+# prediction function   
+@app.route('/predict', methods = ['POST']) 
+def predict(): 
+    A = [float(x) for x in request.form.values()]
+    model_probability = model.predict_proba([A])
+    prediction = "Probability of  this user having Diabetes is %0.2f"%model_probability[0][1]
+    return render_template('index.html', result = prediction)
+if __name__ == "__main__":
+    app.run()
